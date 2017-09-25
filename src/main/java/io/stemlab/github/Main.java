3593cdd781687c31b2545052d7b7a7d899f7730c
dongmin <dongmin.kim@pusan.ac.kr>
2017-09-25 14:27:23 +0900

    add Example

--- src/main/java/io/stemlab/github/Main.java
+++ src/main/java/io/stemlab/github/Main.java
@@ -23,41 +23,6 @@ public class Main {
     //DataLoad
     public static List<Polygon> dataLoad() {
         List<Polygon> polygonList = new ArrayList<Polygon>();
-//
-//        ArrayList<Coord> coords  = new ArrayList<Coord>();
-//
-//        coords.add(new Coord(128.4564528, 37.4977577));
-//        coords.add(new Coord(128.4564815, 37.4976713));
-//        coords.add(new Coord(128.4574888, 37.4979073));
-//        coords.add(new Coord(128.4574226, 37.4980311));
-//        coords.add(new Coord(128.4564528, 37.4977577));
-//        PolygonType pt = new PolygonType(coords);
-//        polygonList.add(pt);
-//
-//        ArrayList<Coord> coords2  = new ArrayList<Coord>();
-//        coords2.add(new Coord(2.0,2.0));
-//        coords2.add(new Coord(2.0,3.5));
-//        coords2.add(new Coord(3.0,3.5));
-//        coords2.add(new Coord(3.0,2.0));
-//        coords2.add(new Coord(2.0,2.0));
-//        PolygonType pt2 = new PolygonType(coords2);
-//        polygonList.add(pt2);
-//
-//        ArrayList<Coord> coords3  = new ArrayList<Coord>();
-//        coords3.add(new Coord(3.3,1.1));
-//        coords3.add(new Coord(1.0,1.1));
-//        coords3.add(new Coord(0.1,2.5));
-//        coords3.add(new Coord(2.7,3.0));
-//        coords3.add(new Coord(4.0,2.0));
-//        coords3.add(new Coord(3.3,1.1));
-//        PolygonType pt3 = new PolygonType(coords3);
-//        polygonList.add(pt3);
-
-//        double[] array1_1 = {1.0, 0.0, 3.0, 2.0, 1.0, 1.0, -1.0 ,0.0 ,1.0 ,0.0};
-//        polygonList.add(makeBuilding(makeArrayList(array1_1)));
-//
-//        double[] array1 = {1.0,0.0,-1.0,0.0,1.0,1.0,3.0,2.0,1.0,0.0};
-//        polygonList.add(makeBuilding(makeArrayList(array1)));
 
         double[] building1 = {128.4423853, 37.4937327, 128.4424685, 37.4937205,
                 128.4424732, 37.4937561, 128.4423935, 37.4937696, 128.4423853, 37.4937327};
@@ -70,6 +35,20 @@ public class Main {
         double[] building2_cw = {128.4564528, 37.4977577, 128.4574226, 37.4980311, 128.4574888, 37.4979073,
                 128.4564815, 37.4976713, 128.4564528, 37.4977577};
         polygonList.add(makeBuilding(makeArrayList(building2_cw)));
+
+        double[] building3 = {127.0272317, 37.5823958, 127.0272795, 37.5823285, 127.0274195, 37.5823779, 127.0274483, 37.5823417,
+                127.0275777, 37.5824108, 127.0276033, 37.5823564, 127.0276843, 37.5823845, 127.0276296, 37.5824571, 127.0277461, 37.5825262,
+                127.0276911, 37.5825957, 127.0272317, 37.5823958};
+        polygonList.add(makeBuilding(makeArrayList(building3)));
+
+        double[] building4 = {127.0265381, 37.5834985, 127.026851, 37.5830764, 127.0279427, 37.5835846,
+                127.027639, 37.5839942, 127.0275059, 37.5839322, 127.0277303, 37.5836296, 127.0274269,
+                37.5834884, 127.0269491, 37.5832659, 127.0267154, 37.5835811, 127.0265381, 37.5834985};
+        polygonList.add(makeBuilding(makeArrayList(building4)));
+
+        double[] building5 = {127.0263526, 37.5833794, 127.0266632, 37.5829637, 127.0268138, 37.5830227,
+                127.0265179, 37.5834538, 127.0263526, 37.5833794};
+        polygonList.add(makeBuilding(makeArrayList(building5)));
 
         return polygonList;
     }
